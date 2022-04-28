610e42a9008efaba7231d5e96770bca41fcc2beb
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2022-04-29 00:13:08 +0200

    Update kuksimuksi.py

--- kuksimuksi.py
+++ kuksimuksi.py
@@ -5,3 +5,4 @@ rohadjon meg nits
 >>>>>>> 5f619a7a80489d4dff39e8db65cf58fd0b42bc9c
 
 j,mnhjmnjhmjhmjhjhhjhjhjbhbvgvbgfccfvvbbnnmm,.mjngfqjijswhtzfasfhatfuauztrqi3uwrioahssfnasjfbgazsifuwarfzwqofrbheaofzgqawer9io
+hihihiha
